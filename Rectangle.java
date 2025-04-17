2cf90d02067766ee96933f811a11b2b213b6ad7f
dogukansendogan <dogukansendogan@posta.mu.edu.tr>
2025-04-17 13:20:39 +0300

    add

--- Rectangle.java
+++ Rectangle.java
@@ -1,19 +1,27 @@
 public class Rectangle extends Shape {
-
-    private int sideA = 0, sideB = 0;
-
-    public Rectangle(String color, int sideA, int sideB) {
+    public Rectangle(String color, int sideA,int sideB) {
         super(color);
         this.sideA = sideA;
-        this.sideB = sideB;
+        this.sideB=sideB;
     }
 
+    @Override
     public void describe() {
-        System.out.println("Side a is " + this.sideA + " Side b is " + this.sideB);
+        System.out.println("Side a is"+this.sideA+"Side B is"+this.sideB);
         super.describe();
     }
 
-    private int sideA=0
+    private int sideA=0 , sideB=0;
 
+    @Override
+    public boolean equals(Object obj) {
+
+        Rectangle r=(Rectangle) obj;
+
+        if(this.sideA==r.sideA && this.sideB==r.sideB && this.getColor().equals(r.getColor());
+        return true;
+    }
+    else{
+        return false;
     }
 }
